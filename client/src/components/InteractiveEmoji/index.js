39cf98264d867302584713d682bc4a5c5b8f2fd0
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-23 21:50:31 -0500

    middle stage of reactifying tutorial

--- client/src/components/InteractiveEmoji/index.js
+++ client/src/components/InteractiveEmoji/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMousePosition } from "./useMousePosition";
 import "./style.css";
 
 const InteractiveEmoji = () => {
+    const [mousePosition, setMousePosition] = useState({
+        x : 0,
+        y : 0
+    });
+
+    function handleMouseMove(ev) { setMousePosition({
+
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientWidth / 2);
+        let radian = Math.atan2(ev.pageX - x, ev.pageY - y);
+        rot_eye = (radian * (180 / Math.PI) * -1) + 0;
+        eye.style.transform = "rotate("+ rot_eye +"deg)";
+        });
+    }
+
     return (
-        <>
-            <div clasName="face">
+        <div className="faceContainer"
+            onMouseMove={(ev)=> handleMouseMove(ev)}
+            >
+            <div className="face">
                 <div className="eyes">
-                    <div className="eye"></div>
-                    <div className="eye"></div>
+                    <div className="eye" style={}></div>
+                    <div className="eye" style={}></div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
