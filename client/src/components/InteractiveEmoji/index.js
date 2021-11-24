fdf38b79680bbc86b5b5df22040f9c0d52e3f083
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-23 19:21:00 -0500

    initial files, boiler plate from tutorial

---
+++ client/src/components/InteractiveEmoji/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./style.css";
+
+const InteractiveEmoji = () => {
+    return (
+        <>
+            <div clasName="face">
+                <div className="eyes">
+                    <div className="eye"></div>
+                    <div className="eye"></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default InteractiveEmoji;
