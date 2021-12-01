bc4ad4145b2f137117c9071249eb530700d88a4d
Hudson Hancock <84539776+hudsonhancock@users.noreply.github.com>
2021-12-01 15:50:00 -0500

    Adds menu html and react component file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import InteractiveEmoji from "./components/InteractiveEmoji";
 import Login from "./components/login/index";
 import Register from "./components/register/index";
+import Menu from "./components/menu/index";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/menu" element={<Menu />} />
           </Routes>
         </div>
       </Router>
