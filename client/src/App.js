bc2aecc8a3316512845c5c06f664c0b487d4d0e6
Hudson Hancock <hudsonhancock@gmail.com>
2021-11-30 19:57:25 -0500

    Import Login component and adds Login to App funct

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import InteractiveEmoji from './components/InteractiveEmoji';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
         {/* <InteractiveEmoji /> */}
+        <Login />
     </div>
   );
 }
