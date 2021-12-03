15150e40def4197d3c39c0a6527dc774e5692607
Jeff Cela <cela.j@labrador-company.com>
2021-12-02 22:07:06 -0500

    missing dependency

--- client/src/components/miscellaneous/SideDrawer.js
+++ client/src/components/miscellaneous/SideDrawer.js
@@ -142,7 +142,7 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
+          emochat
         </Text>
         <div>
           <Menu>
