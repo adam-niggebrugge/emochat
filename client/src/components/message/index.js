f493afdc72a854acd418c45293d11177217a66f8
Sebastian Campos <sebastian.campos321@gmail.com>
2021-12-02 19:47:21 -0500

    3/message room

---
+++ client/src/components/message/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Room = () => {
+    return (
+            <div>
+        <div className="chat_window">
+          <div className="top_menu">
+            <div className="title">Chat</div>
+          </div>
+          <ul className="messages" />
+          <div className="bottom_wrapper clearfix">
+            <div className="message_input_wrapper">
+              <input className="message_input" placeholder="Type your message here..." />
+            </div>
+            <div className="send_message">
+              <div className="icon" />
+              <div className="text">Send</div>
+            </div>
+          </div>
+        </div>
+        <div className="message_template">
+          <li className="message">
+            <div className="avatar" />
+            <div className="text_wrapper">
+              <div className="text" />
+            </div>
+          </li>
+        </div>
+      </div>
+    
+    )
+}
+
+export default Room
